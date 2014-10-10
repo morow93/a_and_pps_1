e9865886d4dd82caf685e870a5b6426e57149bea
testuser0013 <testuser1300@gmail.com>
2014-10-10 12:32:00 +0400

    Finally add simple main programm

---
+++ main.cpp
@@ -0,0 +1,66 @@
+#include "StandartLibs.h"
+#include "CustomLibs.h"
+
+CompositeUnit* createRomanLegion()
+{
+	CompositeUnit* romanLegion = new CompositeUnit;
+
+	for (int i = 0; i<3000; ++i)
+		romanLegion->addUnit(new Infantryman);
+
+	for (int i = 0; i<1500; ++i)
+		romanLegion->addUnit(new Archer);
+
+	for (int i = 0; i<500; ++i)
+		romanLegion->addUnit(new Horseman);
+
+	return romanLegion;
+}
+
+CompositeUnit* createGreekLegion()
+{
+	CompositeUnit* greekLegion = new CompositeUnit;
+
+	for (int i = 0; i<2000; ++i)
+		greekLegion->addUnit(new Infantryman);
+
+	for (int i = 0; i<1000; ++i)
+		greekLegion->addUnit(new Archer);
+
+	for (int i = 0; i<500; ++i)
+		greekLegion->addUnit(new Horseman);
+
+	return greekLegion;
+}
+
+int main()
+{
+	ConcreteMediator mediator;
+
+	ConcreteArmy romanArmy("Roman army", &mediator);
+	ConcreteArmy greekArmy("Greek army", &mediator);
+
+	for (int i = 0; i < 4; ++i)
+		(&romanArmy)->addUnit(createRomanLegion());
+
+	for (int i = 0; i < 5; i++)
+		(&greekArmy)->addUnit(createGreekLegion());
+
+	cout << "Roman army damaging strength is " << (&romanArmy)->getStrength() << endl;
+	cout << "Greek army damaging strength is " << (&greekArmy)->getStrength() << endl;
+
+	mediator.SetArmy1(&romanArmy);
+	mediator.SetArmy2(&greekArmy);
+
+	romanArmy.Send("Are you ready to die, dirty hellens?");
+	greekArmy.Send("Doing is better than saying, nasty legionnaires!");
+
+	cout << "Press \"ENTER\" to exit";
+
+	cin.get();
+
+	(&romanArmy)->CompositeUnit::~CompositeUnit();
+	(&greekArmy)->CompositeUnit::~CompositeUnit();
+
+	return 0;
+}
