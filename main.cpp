84024484728ab91a6c9bf9ab48cbf54bf3267457
morow93 <derchepur@gmail.com>
2014-11-04 19:22:49 +0400

    Fix creating and removing

--- main.cpp
+++ main.cpp
@@ -57,30 +57,30 @@ int main()
 {
 	ConcreteMediator mediator;
 
-	ConcreteArmy romanArmy("Roman army", &mediator);
-	ConcreteArmy greekArmy("Greek army", &mediator);
+	ConcreteArmy* romanArmy = new ConcreteArmy("Roman army", &mediator);
+	ConcreteArmy* greekArmy = new ConcreteArmy("Greek army", &mediator);
 
 	for (int i = 0; i < 4; ++i)
-		(&romanArmy)->addUnit(createRomanLegion());
+		romanArmy->addUnit(createRomanLegion());
 
 	for (int i = 0; i < 5; i++)
-		(&greekArmy)->addUnit(createGreekLegion());
+		greekArmy->addUnit(createGreekLegion());
 
-	cout << "Roman army damaging strength is " << (&romanArmy)->getStrength() << endl;
-	cout << "Greek army damaging strength is " << (&greekArmy)->getStrength() << endl;
+	cout << "Roman army damaging strength is " << romanArmy->getStrength() << endl;
+	cout << "Greek army damaging strength is " << greekArmy->getStrength() << endl;
 
-	mediator.SetArmy1(&romanArmy);
-	mediator.SetArmy2(&greekArmy);
+	mediator.SetArmy1(romanArmy);
+	mediator.SetArmy2(greekArmy);
 
-	romanArmy.Send("Are you ready to die, dirty hellens?");
-	greekArmy.Send("Doing is better than saying, nasty legionnaires!");
+	romanArmy->Send("Are you ready to die, dirty hellens?");
+	greekArmy->Send("Doing is better than saying, nasty legionnaires!");
 
 	cout << "Press \"ENTER\" to exit";
 
 	cin.get();
 
-	(&romanArmy)->CompositeUnit::~CompositeUnit();
-	(&greekArmy)->CompositeUnit::~CompositeUnit();
+	delete romanArmy;
+	delete greekArmy;
 
 	return 0;
 }
